0724d1a1fdb1feaca855f9bb4787e2ae14f2e2bf
William Okech <38021234+wokech@users.noreply.github.com>
2024-08-24 00:15:02 +0300

    Update music_analysis_spotify_3.R

--- r_scripts/music/music_analysis_spotify_3.R
+++ r_scripts/music/music_analysis_spotify_3.R
@@ -1,3 +1,28 @@
-# South African Top
+# A look at various popular songs
 
-# Tshwala Bam / Jerusalema / Tyla....
+# Best African Songs (Punch Nigeria)
+
+# African Queen by 2Face Idibia (Nigeria)
+# Nomakhanjani by Brenda Fassie (South Africa)
+# Malaika by Fadhili Williams (Kenya)
+# Zangalewa by Golden Sounds (Cameroon)
+# Premier Gaou by Magic System (Ivorian)
+# Wombo Lombo by Angelique Kidjo (Benin)
+# Umqombothi by Yvonne Chaka Chaka (South Africa)
+# Coupe Bibamba by Awilo Longomba (Awilo Longomba)
+
+# YouTube Most Streamed
+
+# This is total views but one can also look at views/video, likes, ...
+# Review social blade...
+
+# 1) Burna Boy
+# 2) Diamond Platinumz
+# 3) Wizkid
+# 4) Fally Ipupa
+# 5) Davido
+# 6) Ckay
+# 7) Mr Flavour
+# 8) P Square
+# 9) 
+# 10) 
