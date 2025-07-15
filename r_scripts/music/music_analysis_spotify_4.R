df327bac73b0de1c0e936139cb0a317508d430ee
William Okech <38021234+wokech@users.noreply.github.com>
2025-07-16 02:31:54 +0300

    update music analysis

---
+++ r_scripts/music/music_analysis_spotify_4.R
@@ -0,0 +1,20 @@
+library(tidyverse)
+library(here)
+library(spotifyr)
+library(purrr)
+library(knitr)
+library(ggridges)
+library(ggjoy)
+library(plotly) 
+library(jsonlite)
+library(lubridate)
+library(gghighlight)
+
+## Set up 
+Sys.setenv(SPOTIFY_CLIENT_ID = '', 
+           SPOTIFY_CLIENT_SECRET = '')
+
+access_token <- get_spotify_access_token(scope = '')
+
+harry_styles <- get_artist_audio_features('harry styles')
+joy <- get_artist_audio_features('joy division')
