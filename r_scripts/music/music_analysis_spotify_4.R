8b0e183f9a9808c44da763752d26da18673530ed
William Okech <38021234+wokech@users.noreply.github.com>
2025-07-20 23:41:45 +0300

    rapper not working. May need to use actual API

--- r_scripts/music/music_analysis_spotify_4.R
+++ r_scripts/music/music_analysis_spotify_4.R
@@ -14,7 +14,7 @@ library(gghighlight)
 Sys.setenv(SPOTIFY_CLIENT_ID = '', 
            SPOTIFY_CLIENT_SECRET = '')
 
-access_token <- get_spotify_access_token(scope = '')
+access_token <- get_spotify_access_token()
 
 harry_styles <- get_artist_audio_features('harry styles')
 joy <- get_artist_audio_features('joy division')
