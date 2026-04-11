f2917d9d5af3fd2a19cbcfb74002ad3238027c46
William Okech <38021234+wokech@users.noreply.github.com>
2026-04-11 18:04:21 +0300

    updated music datasets

--- r_scripts/music/soundcharts_work/httr2_soundcharts_africa_1.R
+++ r_scripts/music/soundcharts_work/httr2_soundcharts_africa_1.R
@@ -1,5 +1,7 @@
 # Using httr2 to extract data from Soundcharts API - Africa
 
+####DO NOT RERUN IF COMPLETE############
+
 # Load packages and libraries
 
 # install.packages("httr2")
@@ -14,6 +16,8 @@ library(purrr)
 library(jsonlite)
 library(usethis)
 library(tidyverse)
+library(readxl)
+library(writexl)
 
 # Create/Edit the .Renviron
 # edit_r_environ()
@@ -21,27 +25,436 @@ library(tidyverse)
 client_id <- Sys.getenv("SOUNDCHARTS_CLIENT_ID")
 client_secret <- Sys.getenv("SOUNDCHARTS_CLIENT_SECRET")
 
-# 1) Sofiya - Nzau Mwaki
+# 1) Sofiya Nzau Kenya
 
 # Create and perform the request 
-response <- request("https://customer.api.soundcharts.com/api/v2.25/song/by-isrc/NLRD52339318") %>%
+response_sofiya_nzau <- request("https://customer.api.soundcharts.com/api/v2.25/song/by-isrc/NLRD52339318") |>
   req_headers(
     'x-app-id' = client_id,
     'x-api-key' = client_secret
-  ) %>%
+  ) |>
   req_perform()
 
 # Parse the JSON response
-data <- response %>% 
+data_sofiya_nzau <- response_sofiya_nzau |> 
   resp_body_json()
 
 # Get audio details
 
-audio_features <- data$object$audio
+audio_features_sofiya_nzau <- data_sofiya_nzau$object$audio
 
-audio_features_sofiya_nzau <- as_tibble(audio_features) |>
+audio_features_sofiya_nzau <- as_tibble(audio_features_sofiya_nzau) |>
   mutate(name = "Sofiya Nzau") |>
   mutate(song = "Mwaki") |>
   relocate(name, song)
 
 
+# 2) Tems	Nigeria
+
+# Create and perform the request 
+response_tems <- request("https://customer.api.soundcharts.com/api/v2.25/song/by-isrc/USSM12203789") |>
+  req_headers(
+    'x-app-id' = client_id,
+    'x-api-key' = client_secret
+  ) |>
+  req_perform()
+
+# Parse the JSON response
+data_tems <- response_tems |> 
+  resp_body_json()
+
+# Get audio details
+
+audio_features_tems <- data_tems$object$audio
+
+audio_features_tems <- as_tibble(audio_features_tems) |>
+  mutate(name = "Tems") |>
+  mutate(song = "WAIT FOR U") |>
+  relocate(name, song)
+
+
+
+# 3) Tyla	South Africa
+
+# Create and perform the request 
+response_tyla <- request("https://customer.api.soundcharts.com/api/v2.25/song/by-isrc/USSM12305126") |>
+  req_headers(
+    'x-app-id' = client_id,
+    'x-api-key' = client_secret
+  ) |>
+  req_perform()
+
+# Parse the JSON response
+data_tyla <- response_tyla |> 
+  resp_body_json()
+
+# Get audio details
+
+audio_features_tyla <- data_tyla$object$audio
+
+audio_features_tyla <- as_tibble(audio_features_tyla) |>
+  mutate(name = "Tyla") |>
+  mutate(song = "Water") |>
+  relocate(name, song)
+
+
+
+# 4) Amaarae Ghana
+
+# Create and perform the request 
+response_amaarae <- request("https://customer.api.soundcharts.com/api/v2.25/song/by-isrc/GBUV72200082") |>
+  req_headers(
+    'x-app-id' = client_id,
+    'x-api-key' = client_secret
+  ) |>
+  req_perform()
+
+# Parse the JSON response
+data_amaarae <- response_amaarae |> 
+  resp_body_json()
+
+# Get audio details
+
+audio_features_amaarae <- data_amaarae$object$audio
+
+audio_features_amaarae <- as_tibble(audio_features_amaarae) |>
+  mutate(name = "Amaarae") |>
+  mutate(song = "SAD GIRLZ LUV MONEY - Remix") |>
+  relocate(name, song)
+
+
+
+
+# 5) Sherine Egypt
+
+# Create and perform the request 
+response_sherine <- request("https://customer.api.soundcharts.com/api/v2.25/song/by-isrc/EGA071800047") |>
+  req_headers(
+    'x-app-id' = client_id,
+    'x-api-key' = client_secret
+  ) |>
+  req_perform()
+
+# Parse the JSON response
+data_sherine <- response_sherine |> 
+  resp_body_json()
+
+# Get audio details
+
+audio_features_sherine <- data_sherine$object$audio
+
+audio_features_sherine <- as_tibble(audio_features_sherine) |>
+  mutate(name = "Sherine") |>
+  mutate(song = "كلام عينيه") |>
+  relocate(name, song)
+
+
+
+# 6) Soolking	Algeria
+
+# Create and perform the request 
+response_soolking <- request("https://customer.api.soundcharts.com/api/v2.25/song/by-isrc/FRPZ92324651") |>
+  req_headers(
+    'x-app-id' = client_id,
+    'x-api-key' = client_secret
+  ) |>
+  req_perform()
+
+# Parse the JSON response
+data_soolking <- response_soolking |> 
+  resp_body_json()
+
+# Get audio details
+
+audio_features_soolking <- data_soolking$object$audio
+
+audio_features_soolking <- as_tibble(audio_features_soolking) |>
+  mutate(name = "Soolking") |>
+  mutate(song = "Casanova") |>
+  relocate(name, song)
+
+
+
+# 7) DJ Malvado	Angola
+
+# Create and perform the request 
+response_dj_malvado <- request("https://customer.api.soundcharts.com/api/v2.25/song/by-isrc/PT7EX2400240") |>
+  req_headers(
+    'x-app-id' = client_id,
+    'x-api-key' = client_secret
+  ) |>
+  req_perform()
+
+# Parse the JSON response
+data_dj_malvado <- response_dj_malvado |> 
+  resp_body_json()
+
+# Get audio details
+
+audio_features_dj_malvado <- data_dj_malvado$object$audio
+
+audio_features_dj_malvado <- as_tibble(audio_features_dj_malvado) |>
+  mutate(name = "DJ Malvado") |>
+  mutate(song = "Mussulo") |>
+  relocate(name, song)
+
+
+
+# 8) Salif Keita	Mali
+
+# Create and perform the request 
+response_salif_keita <- request("https://customer.api.soundcharts.com/api/v2.25/song/by-isrc/FRUM72400728") |>
+  req_headers(
+    'x-app-id' = client_id,
+    'x-api-key' = client_secret
+  ) |>
+  req_perform()
+
+# Parse the JSON response
+data_salif_keita <- response_salif_keita |> 
+  resp_body_json()
+
+# Get audio details
+
+audio_features_salif_keita <- data_salif_keita$object$audio
+
+audio_features_salif_keita <- as_tibble(audio_features_salif_keita) |>
+  mutate(name = "Salif Keita") |>
+  mutate(song = "Yamore (feat. Cesária Evora, Benja (NL) & Franc Fala)") |>
+  relocate(name, song)
+
+
+# 9) ElGrandeToto	Morocco
+
+# Create and perform the request 
+response_elgrandetoto <- request("https://customer.api.soundcharts.com/api/v2.25/song/by-isrc/ZA34K2000415") |>
+  req_headers(
+    'x-app-id' = client_id,
+    'x-api-key' = client_secret
+  ) |>
+  req_perform()
+
+# Parse the JSON response
+data_elgrandetoto <- response_elgrandetoto |> 
+  resp_body_json()
+
+# Get audio details
+
+audio_features_elgrandetoto <- data_elgrandetoto$object$audio
+
+audio_features_elgrandetoto <- as_tibble(audio_features_elgrandetoto) |>
+  mutate(name = "ElGrandeToto") |>
+  mutate(song = "love nwantiti (feat. ElGrande Toto) [North African Remix]") |>
+  relocate(name, song)
+
+
+
+# 10) Samara	Tunisia
+
+# Create and perform the request 
+response_samara <- request("https://customer.api.soundcharts.com/api/v2.25/song/by-isrc/TNA0E2200882") |>
+  req_headers(
+    'x-app-id' = client_id,
+    'x-api-key' = client_secret
+  ) |>
+  req_perform()
+
+# Parse the JSON response
+data_samara <- response_samara |> 
+  resp_body_json()
+
+# Get audio details
+
+audio_features_samara <- data_samara$object$audio
+
+audio_features_samara <- as_tibble(audio_features_samara) |>
+  mutate(name = "Samara") |>
+  mutate(song = "Galbi") |>
+  relocate(name, song)
+
+
+# 11) Nitefreak	Zimbabwe
+
+# Create and perform the request 
+response_nitefreak <- request("https://customer.api.soundcharts.com/api/v2.25/song/by-isrc/USZ4V2300268") |>
+  req_headers(
+    'x-app-id' = client_id,
+    'x-api-key' = client_secret
+  ) |>
+  req_perform()
+
+# Parse the JSON response
+data_nitefreak <- response_nitefreak |> 
+  resp_body_json()
+
+# Get audio details
+
+audio_features_nitefreak <- data_nitefreak$object$audio
+
+audio_features_nitefreak <- as_tibble(audio_features_nitefreak) |>
+  mutate(name = "Nitefreak") |>
+  mutate(song = "Gorah") |>
+  relocate(name, song)
+
+
+
+# 12) Joshua Baraka	Uganda
+
+# Create and perform the request 
+response_joshua_baraka <- request("https://customer.api.soundcharts.com/api/v2.25/song/by-isrc/GBX722530003") |>
+  req_headers(
+    'x-app-id' = client_id,
+    'x-api-key' = client_secret
+  ) |>
+  req_perform()
+
+# Parse the JSON response
+data_joshua_baraka <- response_joshua_baraka |> 
+  resp_body_json()
+
+# Get audio details
+
+audio_features_joshua_baraka <- data_joshua_baraka$object$audio
+
+audio_features_joshua_baraka <- as_tibble(audio_features_joshua_baraka) |>
+  mutate(name = "Joshua Baraka") |>
+  mutate(song = "Wrong Places") |>
+  relocate(name, song)
+
+
+
+# 13) Diamond Platnumz	Tanzania
+
+# Create and perform the request 
+response_diamond_platnumz <- request("https://customer.api.soundcharts.com/api/v2.25/song/by-isrc/TCAEB1949188") |>
+  req_headers(
+    'x-app-id' = client_id,
+    'x-api-key' = client_secret
+  ) |>
+  req_perform()
+
+# Parse the JSON response
+data_diamond_platnumz <- response_diamond_platnumz |> 
+  resp_body_json()
+
+# Get audio details
+
+audio_features_diamond_platnumz <- data_diamond_platnumz$object$audio
+
+audio_features_diamond_platnumz <- as_tibble(audio_features_diamond_platnumz) |>
+  mutate(name = "Diamond Platnumz") |>
+  mutate(song = "Tetema (feat. Diamond Platnumz)") |>
+  relocate(name, song)
+
+
+
+# 14) Mia Guisse	Senegal
+
+# Create and perform the request 
+response_mia_guisse <- request("https://customer.api.soundcharts.com/api/v2.25/song/by-isrc/DGA082471335") |>
+  req_headers(
+    'x-app-id' = client_id,
+    'x-api-key' = client_secret
+  ) |>
+  req_perform()
+
+# Parse the JSON response
+data_mia_guisse <- response_mia_guisse |> 
+  resp_body_json()
+
+# Get audio details
+
+audio_features_mia_guisse <- data_mia_guisse$object$audio
+
+audio_features_mia_guisse <- as_tibble(audio_features_mia_guisse) |>
+  mutate(name = "Mia Guisse") |>
+  mutate(song = "Confuse") |>
+  relocate(name, song)
+
+
+
+# 15) Alpha Blondy Ivory Coast
+
+# Create and perform the request 
+response_alpha_blondy <- request("https://customer.api.soundcharts.com/api/v2.25/song/by-isrc/CIA010700001") |>
+  req_headers(
+    'x-app-id' = client_id,
+    'x-api-key' = client_secret
+  ) |>
+  req_perform()
+
+# Parse the JSON response
+data_alpha_blondy <- response_alpha_blondy |> 
+  resp_body_json()
+
+# Get audio details
+
+audio_features_alpha_blondy <- data_alpha_blondy$object$audio
+
+audio_features_alpha_blondy <- as_tibble(audio_features_alpha_blondy) |>
+  mutate(name = "Alpha Blondy") |>
+  mutate(song = "I Wish You Were Here") |>
+  relocate(name, song)
+
+
+# 16) Fally Ipupa Democratic Republic of the Congo
+
+# Create and perform the request 
+response_fally_ipupa <- request("https://customer.api.soundcharts.com/api/v2.25/song/by-isrc/FR9W11903548") |>
+  req_headers(
+    'x-app-id' = client_id,
+    'x-api-key' = client_secret
+  ) |>
+  req_perform()
+
+# Parse the JSON response
+data_fally_ipupa <- response_fally_ipupa |> 
+  resp_body_json()
+
+# Get audio details
+
+audio_features_fally_ipupa <- data_fally_ipupa$object$audio
+
+audio_features_fally_ipupa <- as_tibble(audio_features_fally_ipupa) |>
+  mutate(name = "Fally Ipupa") |>
+  mutate(song = "À Kinshasa (feat. Fally Ipupa)") |>
+  relocate(name, song)
+
+
+# 17) Mulatu Astatke	Ethiopia
+
+# Create and perform the request 
+response_mulatu_astatke <- request("https://customer.api.soundcharts.com/api/v2.25/song/by-isrc/DEG930990148") |>
+  req_headers(
+    'x-app-id' = client_id,
+    'x-api-key' = client_secret
+  ) |>
+  req_perform()
+
+# Parse the JSON response
+data_mulatu_astatke <- response_mulatu_astatke |> 
+  resp_body_json()
+
+# Get audio details
+
+audio_features_mulatu_astatke <- data_mulatu_astatke$object$audio
+
+audio_features_mulatu_astatke <- as_tibble(audio_features_mulatu_astatke) |>
+  mutate(name = "Mulatu Astatke") |>
+  mutate(song = "Tezeta") |>
+  relocate(name, song)
+
+
+# Combine all data and save
+
+combi_africa_audio_features <- rbind(audio_features_sofiya_nzau, audio_features_tems,
+                                     audio_features_tyla, audio_features_amaarae,
+                                     audio_features_sherine, audio_features_soolking,
+                                     audio_features_dj_malvado, audio_features_salif_keita,
+                                     audio_features_elgrandetoto, audio_features_samara,
+                                     audio_features_nitefreak, audio_features_joshua_baraka,
+                                     audio_features_diamond_platnumz, audio_features_mia_guisse,
+                                     audio_features_alpha_blondy, audio_features_fally_ipupa,
+                                     audio_features_mulatu_astatke)
+
+write_xlsx(combi_africa_audio_features, "saved_data/africa_music_audio_features_apr_2026_part_2.xlsx")
--- r_scripts/music/soundcharts_work/httr2_soundcharts_carib_1.R
+++ r_scripts/music/soundcharts_work/httr2_soundcharts_carib_1.R
@@ -1,4 +1,6 @@
-# Using httr2 to extract data from Soundcharts API - Africa
+# Using httr2 to extract data from Soundcharts API - Carib
+
+####DO NOT RERUN IF COMPLETE############
 
 # Load packages and libraries
 
@@ -14,6 +16,8 @@ library(purrr)
 library(jsonlite)
 library(usethis)
 library(tidyverse)
+library(readxl)
+library(writexl)
 
 # Create/Edit the .Renviron
 # edit_r_environ()
@@ -21,24 +25,416 @@ library(tidyverse)
 client_id <- Sys.getenv("SOUNDCHARTS_CLIENT_ID")
 client_secret <- Sys.getenv("SOUNDCHARTS_CLIENT_SECRET")
 
-# 1) Sofiya - Nzau Mwaki
+# 1) Gente De Zona Cuba
 
 # Create and perform the request 
-response <- request("https://customer.api.soundcharts.com/api/v2.25/song/by-isrc/NLRD52339318") %>%
+response_gente_de_zona <- request("https://customer.api.soundcharts.com/api/v2.25/song/by-isrc/GBUM71400955") |>
   req_headers(
     'x-app-id' = client_id,
     'x-api-key' = client_secret
-  ) %>%
+  ) |>
   req_perform()
 
 # Parse the JSON response
-data <- response %>% 
+data_gente_de_zona <- response_gente_de_zona |> 
   resp_body_json()
 
 # Get audio details
 
-audio_features <- data$object$audio
+audio_features_gente_de_zona <- data_gente_de_zona$object$audio
 
-as_tibble(audio_features)
+audio_features_gente_de_zona <- as_tibble(audio_features_gente_de_zona) |>
+  mutate(name = "Gente De Zona") |>
+  mutate(song = "Bailando") |>
+  relocate(name, song)
+
+
+
+# 2) El Alfa	Dominican Republic
+
+# Create and perform the request 
+response_el_alfa <- request("https://customer.api.soundcharts.com/api/v2.25/song/by-isrc/QM7281884731") |>
+  req_headers(
+    'x-app-id' = client_id,
+    'x-api-key' = client_secret
+  ) |>
+  req_perform()
+
+# Parse the JSON response
+data_el_alfa <- response_el_alfa |> 
+  resp_body_json()
+
+# Get audio details
+
+audio_features_el_alfa <- data_el_alfa$object$audio
+
+audio_features_el_alfa <- as_tibble(audio_features_el_alfa) |>
+  mutate(name = "El Alfa") |>
+  mutate(song = "La Romana (feat. El Alfa)") |>
+  relocate(name, song)
+
+
+# 3) Machel Montano	Trinidad and Tobago
+
+# Create and perform the request 
+response_machel_montano <- request("https://customer.api.soundcharts.com/api/v2.25/song/by-isrc/QMUY41500016") |>
+  req_headers(
+    'x-app-id' = client_id,
+    'x-api-key' = client_secret
+  ) |>
+  req_perform()
+
+# Parse the JSON response
+data_machel_montano <- response_machel_montano |> 
+  resp_body_json()
+
+# Get audio details
+
+audio_features_machel_montano <- data_machel_montano$object$audio
+
+audio_features_machel_montano <- as_tibble(audio_features_machel_montano) |>
+  mutate(name = "Machel Montano") |>
+  mutate(song = "All My Love (remix)") |>
+  relocate(name, song)
+
+
+
+# 4) Major Lazer Jamaica
+
+# Create and perform the request 
+response_major_lazer <- request("https://customer.api.soundcharts.com/api/v2.25/song/by-isrc/QMUY41500008") |>
+  req_headers(
+    'x-app-id' = client_id,
+    'x-api-key' = client_secret
+  ) |>
+  req_perform()
+
+# Parse the JSON response
+data_major_lazer <- response_major_lazer |> 
+  resp_body_json()
+
+# Get audio details
+
+audio_features_major_lazer <- data_major_lazer$object$audio
+
+audio_features_major_lazer <- as_tibble(audio_features_major_lazer) |>
+  mutate(name = "Major Lazer") |>
+  mutate(song = "Lean On") |>
+  relocate(name, song)
+
+
+
+# 5) Au/Ra Antigua and Barbuda
+
+# Create and perform the request 
+response_au_ra <- request("https://customer.api.soundcharts.com/api/v2.25/song/by-isrc/NOG841806010") |>
+  req_headers(
+    'x-app-id' = client_id,
+    'x-api-key' = client_secret
+  ) |>
+  req_perform()
+
+# Parse the JSON response
+data_au_ra <- response_au_ra |> 
+  resp_body_json()
+
+# Get audio details
+
+audio_features_au_ra <- data_au_ra$object$audio
+
+audio_features_au_ra <- as_tibble(audio_features_au_ra) |>
+  mutate(name = "Au/Ra") |>
+  mutate(song = "Darkside") |>
+  relocate(name, song)
+
+
+
+# 6) Baha Men	Bahamas
+
+# Create and perform the request 
+response_baha_men <- request("https://customer.api.soundcharts.com/api/v2.25/song/by-isrc/USSC90000001") |>
+  req_headers(
+    'x-app-id' = client_id,
+    'x-api-key' = client_secret
+  ) |>
+  req_perform()
+
+# Parse the JSON response
+data_baha_men <- response_baha_men |> 
+  resp_body_json()
+
+# Get audio details
+
+audio_features_baha_men <- data_baha_men$object$audio
+
+audio_features_baha_men <- as_tibble(audio_features_baha_men) |>
+  mutate(name = "Baha Men") |>
+  mutate(song = "Who Let the Dogs Out") |>
+  relocate(name, song)
+
+
+
+# 7) Amanda Reifer Barbados
+
+# Create and perform the request 
+response_amanda_reifer <- request("https://customer.api.soundcharts.com/api/v2.25/song/by-isrc/USUM72208965") |>
+  req_headers(
+    'x-app-id' = client_id,
+    'x-api-key' = client_secret
+  ) |>
+  req_perform()
+
+# Parse the JSON response
+data_amanda_reifer <- response_amanda_reifer |> 
+  resp_body_json()
+
+# Get audio details
+
+audio_features_amanda_reifer <- data_amanda_reifer$object$audio
+
+audio_features_amanda_reifer <- as_tibble(audio_features_amanda_reifer) |>
+  mutate(name = "Amanda Reifer") |>
+  mutate(song = "Die Hard") |>
+  relocate(name, song)
+
+
+
+# 8) Sui Uzi Belize
+
+# Create and perform the request 
+response_sui_uzi <- request("https://customer.api.soundcharts.com/api/v2.25/song/by-isrc/QZK6Q2035375") |>
+  req_headers(
+    'x-app-id' = client_id,
+    'x-api-key' = client_secret
+  ) |>
+  req_perform()
+
+# Parse the JSON response
+data_sui_uzi <- response_sui_uzi |> 
+  resp_body_json()
+
+# Get audio details
+
+audio_features_sui_uzi <- data_sui_uzi$object$audio
+
+audio_features_sui_uzi <- as_tibble(audio_features_sui_uzi) |>
+  mutate(name = "Sui Uzi") |>
+  mutate(song = "Feel Me") |>
+  relocate(name, song)
+
+
+# 9) Sagcy Dominica
+
+# Create and perform the request 
+response_sagcy <- request("https://customer.api.soundcharts.com/api/v2.25/song/by-isrc/QZDA62014656") |>
+  req_headers(
+    'x-app-id' = client_id,
+    'x-api-key' = client_secret
+  ) |>
+  req_perform()
+
+# Parse the JSON response
+data_sagcy <- response_sagcy |> 
+  resp_body_json()
+
+# Get audio details
+
+audio_features_sagcy <- data_sagcy$object$audio
+
+audio_features_sagcy <- as_tibble(audio_features_sagcy) |>
+  mutate(name = "Sagcy") |>
+  mutate(song = "Te Fuiste") |>
+  relocate(name, song)
+
+
+
+# 10) Mighty Sparrow	Grenada
+
+# Create and perform the request 
+response_mighty_sparrow <- request("https://customer.api.soundcharts.com/api/v2.25/song/by-isrc/NLHR50914165") |>
+  req_headers(
+    'x-app-id' = client_id,
+    'x-api-key' = client_secret
+  ) |>
+  req_perform()
+
+# Parse the JSON response
+data_mighty_sparrow <- response_mighty_sparrow |> 
+  resp_body_json()
+
+# Get audio details
+
+audio_features_mighty_sparrow <- data_mighty_sparrow$object$audio
+
+audio_features_mighty_sparrow <- as_tibble(audio_features_mighty_sparrow) |>
+  mutate(name = "Mighty Sparrow") |>
+  mutate(song = "Only a Fool") |>
+  relocate(name, song)
+
+
+# 11) SAINt JHN	Guyana
+
+# Create and perform the request 
+response_saint_jhn <- request("https://customer.api.soundcharts.com/api/v2.25/song/by-isrc/RUB421901499") |>
+  req_headers(
+    'x-app-id' = client_id,
+    'x-api-key' = client_secret
+  ) |>
+  req_perform()
+
+# Parse the JSON response
+data_saint_jhn <- response_saint_jhn |> 
+  resp_body_json()
+
+# Get audio details
+
+audio_features_saint_jhn <- data_saint_jhn$object$audio
+
+audio_features_saint_jhn <- as_tibble(audio_features_saint_jhn) |>
+  mutate(name = "SAINt JHN") |>
+  mutate(song = "Roses - Imanbek Remix") |>
+  relocate(name, song)
+
+
+
+# 12) Michaël Brun	Haiti
+
+# Create and perform the request 
+response_michael_brun <- request("https://customer.api.soundcharts.com/api/v2.25/song/by-isrc/USUM71804825") |>
+  req_headers(
+    'x-app-id' = client_id,
+    'x-api-key' = client_secret
+  ) |>
+  req_perform()
+
+# Parse the JSON response
+data_michael_brun <- response_michael_brun |> 
+  resp_body_json()
+
+# Get audio details
+
+audio_features_michael_brun <- data_michael_brun$object$audio
+
+audio_features_michael_brun <- as_tibble(audio_features_michael_brun) |>
+  mutate(name = "Michaël Brun") |>
+  mutate(song = "Positivo") |>
+  relocate(name, song)
+
+
+
+# 13) Byron Messia	St Kitts and Nevis
+
+# Create and perform the request 
+response_byron_messia <- request("https://customer.api.soundcharts.com/api/v2.25/song/by-isrc/QZFZ22348066") |>
+  req_headers(
+    'x-app-id' = client_id,
+    'x-api-key' = client_secret
+  ) |>
+  req_perform()
+
+# Parse the JSON response
+data_byron_messia <- response_byron_messia |> 
+  resp_body_json()
+
+# Get audio details
+
+audio_features_byron_messia <- data_byron_messia$object$audio
+
+audio_features_byron_messia <- as_tibble(audio_features_byron_messia) |>
+  mutate(name = "Byron Messia") |>
+  mutate(song = "Talibans") |>
+  relocate(name, song)
+
+
+
+# 14) Crocadile	St Lucia
+
+# Create and perform the request 
+response_crocadile <- request("https://customer.api.soundcharts.com/api/v2.25/song/by-isrc/QZK6N1955548") |>
+  req_headers(
+    'x-app-id' = client_id,
+    'x-api-key' = client_secret
+  ) |>
+  req_perform()
+
+# Parse the JSON response
+data_crocadile <- response_crocadile |> 
+  resp_body_json()
+
+# Get audio details
+
+audio_features_crocadile <- data_crocadile$object$audio
+
+audio_features_crocadile <- as_tibble(audio_features_crocadile) |>
+  mutate(name = "Crocadile") |>
+  mutate(song = "Rincing Target") |>
+  relocate(name, song)
+
+
+
+# 15) Kevin Lyttle	St Vincent and the Grenadines
+
+# Create and perform the request 
+response_kevin_lyttle <- request("https://customer.api.soundcharts.com/api/v2.25/song/by-isrc/USAT20400415") |>
+  req_headers(
+    'x-app-id' = client_id,
+    'x-api-key' = client_secret
+  ) |>
+  req_perform()
+
+# Parse the JSON response
+data_kevin_lyttle <- response_kevin_lyttle |> 
+  resp_body_json()
+
+# Get audio details
+
+audio_features_kevin_lyttle <- data_kevin_lyttle$object$audio
+
+audio_features_kevin_lyttle <- as_tibble(audio_features_kevin_lyttle) |>
+  mutate(name = "Kevin Lyttle") |>
+  mutate(song = "Turn Me On") |>
+  relocate(name, song)
+
+
+
+# 16) Jairzinho	Suriname
+
+# Create and perform the request 
+response_jairzinho <- request("https://customer.api.soundcharts.com/api/v2.25/song/by-isrc/TCACX1714065") |>
+  req_headers(
+    'x-app-id' = client_id,
+    'x-api-key' = client_secret
+  ) |>
+  req_perform()
+
+# Parse the JSON response
+data_jairzinho <- response_jairzinho |> 
+  resp_body_json()
+
+# Get audio details
+
+audio_features_jairzinho <- data_jairzinho$object$audio
+
+audio_features_jairzinho <- as_tibble(audio_features_jairzinho) |>
+  mutate(name = "Jairzinho") |>
+  mutate(song = "Tempo") |>
+  relocate(name, song)
+
+
+
+
+
+# Combine all data and save
+
+combi_carib_audio_features <- rbind(audio_features_gente_de_zona, audio_features_el_alfa,
+                                     audio_features_machel_montano, audio_features_major_lazer,
+                                     audio_features_au_ra, audio_features_baha_men, 
+                                     audio_features_amanda_reifer, audio_features_sui_uzi,
+                                     audio_features_sagcy, audio_features_mighty_sparrow,
+                                     audio_features_saint_jhn, audio_features_michael_brun,
+                                     audio_features_byron_messia, audio_features_crocadile,
+                                     audio_features_kevin_lyttle, audio_features_jairzinho)
+
+write_xlsx(combi_carib_audio_features, "saved_data/carib_music_audio_features_apr_2026_part_2.xlsx")
 
 
